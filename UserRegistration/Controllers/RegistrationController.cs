fbe2688e0f96f38cec1c780c93b309067c8d030e
Nandhu004 <nandhuaish2000@gmail.com>
2021-04-23 16:08:34 +0530

    Initial commit

---
+++ UserRegistration/Controllers/RegistrationController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserRegistration.Models;
+
+namespace UserRegistration.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private readonly ApplicationUser _auc;
+
+        public RegistrationController(ApplicationUser auc)
+        {
+            _auc = auc;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(User uc)
+        {
+            _auc.Add(uc);
+            _auc.SaveChanges();
+            ViewBag.message = "The user " + uc.Username + " is saved successfully"; 
+        return View();
+        }
+    }
+}
---
+++ UserRegistration/Models/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserRegistration.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Username..")]
+        [Display(Name = "UserName")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Password...")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Pwd { get; set; }
+
+        [Required(ErrorMessage = "Please Enter the Confirm Password...")] 
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Pwd")]
+        public string Confirmpwd { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Email...")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Select the Gender...")]
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Select the Location...")]
+        [Display(Name = "Location")]
+        public string Location { get; set; }
+        public string Address { get; set; }
+
+
+    }
+}
